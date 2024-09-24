ddb201c3e1b0af33fb3f64de8f9683116050063e
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-09-24 10:42:40 +0530

    minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'True Feedback',
-  description: 'Real feedback from real people.',
+  title: 'OpenVoice',
+  description: 'Voice Openly',
 };
 
 interface RootLayoutProps {
